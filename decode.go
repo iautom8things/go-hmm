f1b39b205e14e6447f062d2daafa8bac5bd7805e
Manuel Zubieta <mazubieta@gmail.com>
2014-06-09 23:32:14 -0500

    removed debugging lines

--- decode.go
+++ decode.go
@@ -6,9 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"git.bigodev.com/mazubieta/go-hmm/markov"
-	"io"
-	"io/ioutil"
-	"log"
 	"math"
 	"math/Rand"
 	"os"
@@ -16,37 +13,7 @@ import (
 	"time"
 )
 
-var (
-	TRACE   *log.Logger
-	INFO    *log.Logger
-	WARNING *log.Logger
-	ERROR   *log.Logger
-)
-
-func Init(
-	traceHandle io.Writer,
-	infoHandle io.Writer,
-	warningHandle io.Writer,
-	errorHandle io.Writer) {
-
-	TRACE = log.New(traceHandle,
-		"TRACE: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	INFO = log.New(infoHandle,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	WARNING = log.New(warningHandle,
-		"WARNING: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	ERROR = log.New(errorHandle,
-		"ERROR: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-}
 func main() {
-	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 	var seed int64
 	var numIter int
 	var transProb float64
@@ -136,7 +103,6 @@ func main() {
 
 	for i := 0; i < 1; i++ {
 		observation := observations[i]
-		INFO.Println("workong on observation: ", observation)
 		states := hiddenStates[i]
 
 		if len(observation) != len(states) {
@@ -150,7 +116,6 @@ func main() {
 		var max_state string
 		max_val := math.Inf(-1)
 		for _, s := range model.States {
-			INFO.Println("working on initial state: ", s.Name)
 			v[s.Name] = make([]float64, len(observation))
 			e, err := s.GetEmitionProbability(string(observation[0]))
 			if err != nil {
@@ -159,7 +124,6 @@ func main() {
 			t := model.GetInitialProb(s.Name)
 			v_j := math.Exp(math.Log(e) + math.Log(t))
 			v[s.Name][0] = v_j
-			INFO.Println("state:", s.Name, "e_prob:", e, "t_prob:", t, "prev_prob:", v[s.Name][0])
 			if v_j > max_val {
 				max_val = v_j
 				max_state = s.Name
@@ -167,18 +131,13 @@ func main() {
 		}
 		pi[0] = max_state
 
-		INFO.Println("ITER OVER OBS")
 		for j := 1; j < len(observation); j++ {
 			obs_j := string(observation[j])
-			INFO.Println("obs: <", obs_j, ">")
 
 			var max_j_state string
 			max_j_val := math.Inf(-1)
 			for _, state_j := range model.States {
-				INFO.Println("Trying to beat maxes: ", max_j_state, "v:", max_j_val)
 				e_j, _ := state_j.GetEmitionProbability(obs_j)
-
-				WARNING.Println("state: <", state_j.Name, "> emit_p: ", e_j)
 
 				max_i_val := math.Inf(-1)
 				for _, state_i := range model.States {
@@ -187,8 +146,6 @@ func main() {
 
 					mul := math.Exp(math.Log(v_prev) + math.Log(t_prob))
 
-					INFO.Println("State: ", state_i.Name, " v_prev: ", v_prev, "t_prob:", t_prob, "mul: ", mul)
-
 					if mul > max_i_val {
 						max_i_val = mul
 					}
@@ -196,11 +153,8 @@ func main() {
 
 				v_j := math.Exp(math.Log(e_j) + max_i_val)
 
-				WARNING.Println("See if we beat maxes with: v_j: <", v_j)
-
 				v[state_j.Name][j] = v_j
 				if v_j > max_j_val {
-					INFO.Println("OLD: ", max_j_state, "v:", max_j_val)
 					max_j_val = v_j
 					max_j_state = state_j.Name
 				}
@@ -209,7 +163,11 @@ func main() {
 		}
 
 		fmt.Println(states)
-		fmt.Println(pi)
+		var piBuffer bytes.Buffer
+		for _, p := range pi {
+			piBuffer.WriteString(p)
+		}
+		fmt.Println(piBuffer.String())
 
 	}
 }
